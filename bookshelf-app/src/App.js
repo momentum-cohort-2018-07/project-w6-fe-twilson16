65070896d57e21faf18bbb588d84944ce0d735d8
Tina <christinawilson6@gmail.com>
2018-09-10 09:27:08 -0400

    can go to edit page but then tried to do too much at once so nothing else works

--- bookshelf-app/src/App.js
+++ bookshelf-app/src/App.js
@@ -1,15 +1,18 @@
 import React, { Component } from 'react'
 import Book from './book.js'
-// import CondensedView from './CondensedView.js'
+import EditingView from './EditingView.js'
 import './App.css'
 import request from 'superagent'
+import AddBookView from './AddBookView.js'
 
 class App extends Component {
   constructor () {
     super()
     this.state = {
       expanded: false,
-      books: []
+      books: [],
+      editing: null,
+      adding: null
     }
   }
 
@@ -22,14 +25,44 @@ class App extends Component {
       })
   }
 
+  updateBookInApi (book, id) {
+    request.put('http://localhost:4000/books/id')
+      .send(book)
+  }
+
+  editingBook (e, book) {
+    this.setState({
+      editing: {book}
+    })
+  }
+
+  addingBook (e, book) {
+    this.setState({
+      adding: {book}
+    })
+  }
+
+  updateBook (e, book, id) {
+    const bookId = this.book.id
+    this.updateBookInApi(book, bookId)
+  }
+
   render () {
+    if (this.state.editing) {
+      return (<EditingView book={this.state.editing} />)
+    }
+
+    if (this.state.adding) {
+      return (<AddBookView book={this.state.adding} />)
+    }
+
     return (
       <div className='App'>
         <header className='App-header'>
           <h1 className='App-title'>Bookshelf App</h1>
         </header>
         <div className='section'>
-          {this.state.books.map((book, idx) => <Book key={idx} book={book} />)}
+          {this.state.books.map((book, idx) => <Book key={idx} book={book} editingBook={this.editingBook.bind(this)} />)}
         </div>
       </div>
     )
--- bookshelf-app/src/book.js
+++ bookshelf-app/src/book.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react'
-// import books from './books.json'
+import PropTypes from 'prop-types'
+// import App from './App.js'
+// import EditingView from './EditingView.js'
 
 class Book extends Component {
   constructor (props) {
@@ -22,22 +24,37 @@ class Book extends Component {
           <div className='book-title'><span className='bold'>Title:</span> {book.title}</div>
           <div className='book-cover'><img className='book-image' src={book.coverImageUrl} alt='Book Cover' /></div>
           <div className='author'><span className='bold'>Author:</span> {book.author}</div>
-          <div className='short-description'><span className='bold'>Short Description:</span> {book.shortDescription}</div>
+          <div className='short-description'><span className='bold'>Short Description:</span>{book.shortDescription}</div>
         </div>
         {this.state.expand ? (
           <div className='expanded-info-div' data-expanded-state='false'>
-            <div className='button-div'><button className='expand-button' onClick={() => this.handleClick()}>&#x25BC;  Less Information</button></div>
+            <div className='button-div'><button className='collapse-button' onClick={() => this.handleClick()}>&#x25BC;  Less Information</button></div>
             <div className='book-Url'><span className='bold'>URL:</span> {book.url}</div>
             <div className='publisher'><span className='bold'>Publisher:</span> {book.publisher}</div>
             <div className='publication-date'><span className='bold'>Publication Date:</span> {book.publicationDate}</div>
             <div className='expanded-description'><span className='bold'>Full description:</span> <p>{book.detailedDescription}</p></div>
-            <div className='editing'><button className='editing-button'>Edit</button></div>
+            <div className='editing'><button className='editing-button' onClick={(e) => this.props.editingBook(e, book)}>Edit</button></div>
+            <div className='adding'><button className='adding-button' onClick={(e) => this.props.addingBook(e, book)}>Add Book</button></div>
           </div>
         )
-          : <div className='button-div'><button className='expand-button' onClick={() => this.handleClick()}>&#x25B6; More Information</button></div>}
+          : <div className='button-div'><button className='expand-button' onClick={this.handleClick}>&#x25B6; More Information</button></div>}
       </div>
     )
   }
+}
+
+Book.PropTypes = {
+  book: PropTypes.arrayOf(PropTypes.shape({
+    title: PropTypes.string,
+    coverImageUrl: PropTypes.img,
+    author: PropTypes.string,
+    shortDescription: PropTypes.string,
+    url: PropTypes.url,
+    publisher: PropTypes.string,
+    publicationDate: PropTypes.string,
+    detailedDescription: PropTypes.string
+  })).isRequired,
+  handleClick: PropTypes.func
 }
 
 export default Book
